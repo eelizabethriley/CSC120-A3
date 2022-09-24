4798a58bee66e281961370a72eb0d7bb6a279aac
eelizabethriley <eeriley@smith.edu>
2022-09-24 14:51:10 -0400

    Minor changes.

--- Conversation.java
+++ Conversation.java
@@ -15,20 +15,24 @@ class Conversation {
 
     // Start the conversation with a greeting and a question.
     System.out.println("Hi there! What's on your mind?");
-    for (int i = 0; i < rounds+1; i++) {
+    // Continue the conversation for the number of rounds inputted.
+    for (int i = 0; i <= rounds; i++) {
+      //  Take in the user response for each round, checking for mirror words.
       String userInput = chatResponse.nextLine();
       if (userInput.contains("I")) {
         userInput = "you";
+        System.out.println("you + rest of line"+"?");
       } else if (userInput.contains("you")) {
         // turn the token of the input that = you into I
         userInput = "I";
+        System.out.println("I + rest of the line"+"?");
       } else {
         System.out.println("Okay.");
       }
 
     }
+    chatResponse.close();
     System.out.println("Goodbye!");
-
     // Take in the input given by the user in response
     // If the input contains mirror words, convert the words to their mirror and return the altered string.
     // Otherwise, return a canned response.
